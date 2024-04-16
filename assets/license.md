ce9b37bbfc9c55921e4e084f5248cb547c145dd7
Cora <silvative@gmail.com>
2024-04-16 15:20:10 +0100

    Replace audio files

---
+++ assets/license.md
@@ -0,0 +1,5 @@
+This module uses the following sound effects from 'Medieval Fantasy SFX 2' by Ovani Sound
+ - "Equip Armor.wav"
+ - "Potion Open A.wav"
+
+They are used in this module in accordance with the pack's royalty-free license.
--- scripts/audiocues.js
+++ scripts/audiocues.js
@@ -1,38 +1,31 @@
-const NOTIFICATIONSOUND = new Audio("/modules/accessibility-enhancements/assets/ding.mp3");
-const CREATEITEMSOUND = new Audio("/modules/accessibility-enhancements/assets/yoink.mp3");
+const NOTIFICATIONSOUND = new Audio("/modules/accessibility-enhancements/assets/open.wav");
+const CREATEITEMSOUND = new Audio("/modules/accessibility-enhancements/assets/equip.wav");
 
 Hooks.on("init", () => {
-
     game.settings.register('accessibility-enhancements', 'enableSoundEffects', {
         name: 'Enable Sound Effects',
-        hint: 'Disable to stop the "ping" sound on rendered applications.',
+        hint: 'Disable to stop the "pop" sound when rendering applications and the rustling sound when items are created.',
         scope: 'client',
         config: true,
         type: Boolean,
         default: false,
-        onChange: value => {},
+        onChange: value => { },
     });
 
 })
 
-
-
 Hooks.on("renderApplication", (element) => {
-
     // Play a sound when a window opens
     if ( element.template !== "systems/pf2e/templates/system/effects-panel.hbs" ) {
         if ( game.settings.get('accessibility-enhancements', 'enableSoundEffects') === true ) {
             NOTIFICATIONSOUND.play();
         }
     }
-
 })
 
 Hooks.on("createItem", () => {
-
     // Play a sound when an item is created
     if ( game.settings.get('accessibility-enhancements', 'enableSoundEffects') === true ) {
         CREATEITEMSOUND.play();
     }
-
 })
